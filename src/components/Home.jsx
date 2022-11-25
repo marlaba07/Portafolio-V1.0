a7d4380d1c59dc8355c9bf16744e33f77dfb4531
Mariano Labatut <marianolabatut@gmail.com>
2022-11-25 18:24:20 -0300

    Skills

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
                 <p className='text-red-500'> Hi, my Name is</p>
                 <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'> Mariano Labatut </h1>
                 <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'> I'm a Frontend Developer </h2>
-                <p className='text-[#8892b0 py-4 max-w-[700px]'>
+                <p className='text-[#8892b0] py-4 max-w-[700px]'>
                     I'm a full-stack developer specializing in builiding (and occasionally designing)
                     exceptional digital experiences. Currently, i'm docused on builiding responsive fullstack web applications
                 </p>
